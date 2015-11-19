d854b45ffd060ede0c075ca82d0cde9091e35121
70060463 <70060463@Info09PC17.CASTELLI.LOCAL>
2015-11-19 12:51:20 +0100

    added some files

---
+++ JXG/src/jxg/JXG.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jxg;
+
+/**
+ *
+ * @author 70060463
+ */
+public class JXG {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
